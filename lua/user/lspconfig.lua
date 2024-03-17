9723048feadba280413545c058e016c8951c647e
Rizwan Elansyah <111105347+Rizwanelansyah@users.noreply.github.com>
2024-03-18 06:15:30 +0700

    Update lspconfig.lua

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -1,7 +1,7 @@
 local lsp = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local servers = { "lua_ls", "tsserver", "pyright", "ccls" }
+local servers = { "lua_ls", "tsserver", "pyright" }
 for _, server in ipairs(servers) do
 	lsp[server].setup({
 		capabilities = capabilities,
